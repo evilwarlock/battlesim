56d09ed3577c3a5947b03c0fbdc55d543a525827
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2021-07-20 22:26:18 -0400

    Update battlesim.py
    make mid * 1, not 2 if the ship been attacked is a tank, and change the format of the fleet

--- battlesim.py
+++ battlesim.py
@@ -16,7 +16,7 @@ class Ship:
         self.role = role
 
     def isAlive(self):
-        if self.ehp <= 0:
+        if self.ehp < 0:
             return False
         else:
             return True
@@ -40,10 +40,37 @@ class Ship:
             return False
 
 
-class Fleet:
-    def __init__(self, ships):
-        self.ships = ships
+# Ship(dps, high_slot_hps, mid_slot_hps, ehp, role, timer)
+# the number is divided by resist
 
+
+def dps():
+    return Ship(1500, 0, 387, 250000, 'Dps', 0)
+
+
+def healer():
+    return Ship(0, 3377, 309, 100000, 'Healer', 0)
+
+
+def tanker():
+    return Ship(300, 0, 0, 250000, 'Tanker', 0)
+
+class Fleet:
+    # type is the number of ship in order of healer, tank, dps, pm = primary mode is the order that ship gets attackted
+    def __init__(self, pm, t, h, d):
+        self.ships = []
+        for o in pm:
+            if (o == 2):
+                for i in range(h):
+                    self.ships.append(healer())
+            if (o == 1):
+                for i in range(t):
+                    self.ships.append(tanker())
+            if (o == 3):
+                for i in range(d):
+                    self.ships.append(dps())
+
+            
     def hasTanker(self):
         for ship in self.ships:
             if ship.isAlive():
@@ -65,17 +92,23 @@ class Fleet:
                 dps += ship.dps
         return dps
 
+    def get_attacked_ship(self):
+        for ship in self.ships:
+            if ship.isAlive():
+                return ship
+        return None
+
     def get_total_hps(self):
         hps = 0
         healer_being_attacked = True
         for ship in self.ships:
             if ship.isAlive():
-                if self.hasTanker() and self.hasHealer():
+                if self.hasTanker() and self.hasHealer() and (self.get_attacked_ship().isTanker() == False):
                     hps += ship.mid_slot_hps * 2
                 else:
                     hps += ship.mid_slot_hps
                 if ship.isHealer():
-                    if healer_being_attacked:
+                    if (self.get_attacked_ship().isHealer() == True) and healer_being_attacked:
                         healer_being_attacked = False
                     else:
                         hps += ship.high_slot_hps
@@ -86,21 +119,6 @@ class Fleet:
         print('Total HPS after this round:' + str(self.get_total_hps()))
         for ship in self.ships:
             print(ship.role + ' ' + str(ship.ehp))
-
-# Ship(dps, high_slot_hps, mid_slot_hps, ehp, role, timer)
-# the number is divided by resist
-
-
-def dps():
-    return Ship(1500, 0, 387, 250000, 'Dps', 0)
-
-
-def healer():
-    return Ship(0, 3377, 309, 100000, 'Healer', 0)
-
-
-def tanker():
-    return Ship(300, 0, 0, 250000, 'Tanker', 0)
 
 
 def simulation(fleet1, fleet2):
@@ -130,10 +148,9 @@ def simulation(fleet1, fleet2):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    f1 = Fleet([tanker(), dps(), dps(), dps(), dps(), dps(), dps(), dps(), dps(), dps()])
-    f2 = Fleet([healer(), healer(), healer(), tanker(), dps(), dps(), dps(), dps(), dps(), dps()])
-    #f2 = Fleet([tanker(), dps(), dps(), dps(), dps(), dps(), dps(), dps(), dps(), dps()])
-    for i in range(0, 1000):
+    f1 = Fleet((2, 1, 3), 0, 0, 1)
+    f2 = Fleet((2, 1, 3), 1, 2, 0)
+    for i in range(1000):
         print('duration: ' + str(i))
         simulation(f1, f2)
         print('status of Fleet1')
